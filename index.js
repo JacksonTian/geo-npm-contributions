daae43977d2551a1ae874efa696ef93751e3553d
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-06 20:09:50 -0500

    caps cities

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ fs.readFile('./modulesArray.json', function(err,modules){
   var cities = JSON.parse(modules)
   _.each(cities, function(city){
     //console.log(city.city.split(',')[0])
-    city.capsCity = city.city.split(',')[0]
+    city.capsCity = city.city.split(',')[0].toUpperCase()
   })
   console.log(cities)
   fs.writeFile('./formattedCity.json', JSON.stringify(cities), function(err){
