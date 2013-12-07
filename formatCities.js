85082bce5d2cbae8e41edda0df5087df97742dd3
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-06 20:23:38 -0500

    finalized

---
+++ formatCities.js
@@ -0,0 +1,16 @@
+var _ = require('lodash'),
+    turf = require('turf'),
+    fs = require('fs')
+
+console.log('reading')
+fs.readFile('./modulesArray.json', function(err,modules){
+  var cities = JSON.parse(modules)
+  _.each(cities, function(city){
+    //console.log(city.city.split(',')[0])
+    city.capsCity = city.city.split(',')[0].toUpperCase()
+  })
+  console.log(cities)
+  fs.writeFile('./formattedCity.json', JSON.stringify(cities), function(err){
+    console.log('complete')
+  })
+})
