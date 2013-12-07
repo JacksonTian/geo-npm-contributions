2da6baa360f57f7d02a07642ad9848660a55bc7a
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-06 20:07:47 -0500

    caps cities

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var _ = require('lodash'),
     turf = require('turf'),
     fs = require('fs')
 
-fs.readFile('./cities.geojson', function(cities){
-  
+console.log('reading')
+fs.readFile('./modulesArray.json', function(err,modules){
+  var cities = JSON.parse(modules)
+  _.each(cities, function(city){
+    //console.log(city.city.split(',')[0])
+    city.capsCity = city.city.split(',')[0]
+  })
+  console.log(cities)
+  fs.writeFile('./formattedCity.json', JSON.stringify(cities), function(err){
+    console.log('complete')
+  })
 })
